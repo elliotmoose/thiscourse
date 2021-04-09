c518167e4a4384ecc3994c65bee5de805e1ad142
Viet Pham <victorpham1997@gmail.com>
2021-04-09 12:34:51 +0800

    Added component folder

---
+++ web-app/src/components/CreateSession.js
@@ -0,0 +1,21 @@
+import React from 'react';
+ 
+const CreateSession = () => {
+    return (
+       <div className="CreateSession">
+			<h2>Create Session</h2>
+			<br></br>
+			<br></br>
+			<br></br>
+
+			<p>Home page body content</p>
+			<form>
+			  <label>
+			    <input className="input-box" type="text" name="name" />
+			  </label>
+			</form>
+       </div>
+    );
+}
+ 
+export default CreateSession;
